2e6b463c72b64e1d4ecaec1bdb02aa8e7c036e4b
Joonas Praks <joonaspraks@gmail.com>
2022-09-02 16:52:11 +0300

    init Project setup

---
+++ src/main/java/com/yotproov/YotproovApplication.java
@@ -0,0 +1,13 @@
+package com.yotproov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YotproovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YotproovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yotproov/yotproov/YotproovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yotproov.yotproov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YotproovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
