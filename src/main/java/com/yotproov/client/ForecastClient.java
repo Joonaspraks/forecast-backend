245b4fe779162210100a06a06bdb769317bf231c
Joonas Praks <joonaspraks@gmail.com>
2022-09-06 18:30:29 +0300

    using xjc for xsd to model generation

--- src/main/java/com/yotproov/client/ForecastClient.java
+++ src/main/java/com/yotproov/client/ForecastClient.java
@@ -1,17 +1,22 @@
 package com.yotproov.client;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.yotproov.model.Forecast;
+import generated.Forecasts;
+import generated.Observations;
 
 @Service
 public class ForecastClient {
     RestTemplate template = new RestTemplate();
 
-    public ResponseEntity<Forecast> getForecast() {
-        return template.getForEntity("https://www.ilmateenistus.ee/ilma_andmed/xml/forecast.php?lang=eng",
-                Forecast.class);
+    public Forecasts getForecast() {
+        return template.getForObject("https://www.ilmateenistus.ee/ilma_andmed/xml/forecast.php?lang=eng",
+                Forecasts.class);
+    }
+
+    public Observations getObservations() {
+        return template.getForObject("https://www.ilmateenistus.ee/ilma_andmed/xml/observations.php",
+                Observations.class);
     }
 }
--- src/main/java/com/yotproov/model/Forecast.java
+++
@@ -1,8 +0,0 @@
-package com.yotproov.model;
-
-import lombok.Data;
-
-@Data
-public class Forecast {
-    
-}
